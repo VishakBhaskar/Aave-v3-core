c919a7b7e05b18f9e13f5a0b476cd1375be1a264
Vishak Bhaskar <vishakbhaskar.619@gmail.com>
2022-12-16 23:03:13 +0530

    Listed out all the core contract addresses

--- addresses.js
+++ addresses.js
@@ -1,7 +1,12 @@
-// SequencerOracle deployed to: 0x44d3CfFc4F66e33A06c383d4447247aE7d6C48b7
-// PoolAddressesProvider deployed to : 0x27E9b7f952C2cDcF64F6bf57136fEEF1d3Ed88DA
-// ACLManager deployed to : 0xda9187093947EA00CbA8AC395512cCD54661845e
-// PoolAddressesProviderRegistry deployed to : 0x4786986B0dcC0A79A9A20fbD3F4a11Dd10caBD61
-// PriceOracleSentinel deployed to : 0xA80426F1df5385312c477211029CDA520BA53786
-// DefaultReserveInterestRateStrategy deployed to : 0x8F9808637F66dB1Dc7052D47b8312593a2C9CD3e
-// PoolStorage deployed to : 0xAa41746A391596D465E6f112cB6665A6477e518E
+// SequencerOracle deployed to: 0xd2Cf02FA1B66eeAee512ea141Cf967a11D04fca7
+// PoolAddressesProvider deployed to : 0x34B93b84c9fE40F7Ca762B4607c943E292dF21FC
+// ACLManager deployed to : 0x93720FFA46e57713bE0EBC16C19F25353840a837
+// PoolAddressesProviderRegistry deployed to : 0xAB2300849EE94e203185BB3E7B21b2C84C9658c9
+// PriceOracleSentinel deployed to : 0xf09b47914680C790c6bfc349e1b86bED35cE33Ef
+// DefaultReserveInterestRateStrategy deployed to : 0x41f0123ec26b05BF5a1254eF34017FA2B49dd211
+// PoolStorage deployed to : 0x78c212F7d552B7251394B63d15ad34f98e86aCe6
+// AaveProtocolDataProvider deployed to : 0xbeBA3090825a67f28059c6953Ca2A331cFfD18e3
+// AToken deployed to : 0x977B9b2403e50B00208482D21DF6CD162d3eE0b8
+// DelegationAwareAToken deployed to : 0xA080197f90B73500Fadf91e7261270d4CA2889C7
+// StableDebtToken deployed to : 0x1beda909E0Dc7CFfA57d756357a1762D38a064B1
+// VariableDebtToken deployed to : 0xc46f383541FA9551b059A81124B7fEbe677255Bd
